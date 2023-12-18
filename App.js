cb28f32719d9aa209c4bbb2ab89ece787d61a99d
Goutham-ps <154111935+Goutham-ps@users.noreply.github.com>
2023-12-18 09:28:34 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Loginsignup from './Components/Loginsignup';
+
+function App() {
+  return (
+    <div >
+     <Loginsignup/>
+     
+    
+    </div>
+  );
+}
+
+export default App;
